b31a583a3104cb227891bff9c5a00fc65893e2ab
prikhodkin <info@prikhodkin.com>
2019-12-12 22:45:40 +0500

    projects page mobile version

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,3 +4,6 @@ $('.main-menu__btn').on('click', function () {
   $('.main-menu__list').slideToggle('slow');
   $('.main-menu__btn').toggleClass('active');
 });
+$('.filter__show').on('click', function () {
+  $('.filter__form').slideToggle('slow');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,7 @@ $('.main-menu__btn').on('click', function () {
   $('.main-menu__list').slideToggle('slow');
   $('.main-menu__btn').toggleClass('active');
 });
+
+$('.filter__show').on('click', function () {
+  $('.filter__form').slideToggle('slow');
+});
